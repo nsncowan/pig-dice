dfd1ad03720d20418aafa7b775ae5ca85dcd31aa
Noah Cowan <nsncowan@gmail.com>
2022-09-18 16:35:02 -0700

    add js code and html.
    Co-authored-by: Athea DeLing <almdeling@gmail.com>

--- README.md
+++ README.md
@@ -11,8 +11,30 @@
 * a counter to count the number of rolls per turn (and/or rolls total)
 * a function the returns a game over/winner message when a player reaches 100
 
+
+
 ### TDD
 
 Test: It should output a value between one and six when diceRoll is called
 Code: let diceRoll = value
 Expected Output: number between 1 and 6
+
+Test: it should add diceRoll output to currentTurn variable
+code: let diceRoll = value; let score = Number
+expected output: Number + value
+
+Test: It should continue players turn when any number other than one is rolled
+Code: playerRoll = value
+Expected Output: number rolled
+
+Test: It should stop players turn when one is rolled
+Code: playerRoll = 1
+Expected Output: 0
+
+Test: it should add currentTurn number to player score
+Code:
+Expected Output:
+
+Test: It should add currentTurn to player 1
+Code: addscore(1)
+Expected Output: 
--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,49 @@
 // business logic
 
-//player constructor
-function Player(name, score, turnScore) {
-  this.name = name;
-  this.score = score;
-  this.turnScore = turnScore;
+/*
+function activePlayer(){
+
 }
 
+function playerOne() {
+  score = playerOneScore
+  activePlayer = true
+}
 
+function playerTwo() {
+  score = playerTwoScore
+  activePlayer = false
+}
+*/
+
+let playerOneScore = document.getElementById("playerOneScore");
+let playerTwoScore = document.getElementById("playerTwoScore");
+let currentTurn = document.getElementById("currentTurn");
+let playerOne = 1
 
 // dice variable
-let diceRoll = Math.floor(Math.random() * 6) + 1;
+
+function playerRoll() {
+  let roll = Math.floor(Math.random() * 6) + 1;
+  if (roll !== 1) {
+    currentTurn += roll;
+  } else {
+      currentTurn = 0;
+  }
+}
+
+function addScore() {
+  if (playerOne = 1) {
+    playerOneScore += currentTurn;
+    playerOne = 0;
+  } else (playerOne = 0) {
+    playerTwoScore += currentTurn;
+    playerOne = 1;
+  }
+}
+// UI logic
+
+// eventlistener for "roll dice" button = run playerRoll()
+// eventlistener for "endturn" button = run addScore()
+
+
